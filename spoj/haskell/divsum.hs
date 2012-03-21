769019ef9930d3a3374c4c7f06ce45e1e1827d26
Andrew Korzhuev <wolfon@gmail.com>
2012-03-21 06:08:54 +0400

    better algo, still TLE tho

--- spoj/haskell/divsum.hs
+++ spoj/haskell/divsum.hs
@@ -1,20 +1,24 @@
 import Data.Maybe
 import qualified Data.ByteString.Lazy.Char8 as BS
 
-intSqrt :: Int -> Int
-intSqrt = floor . sqrt . fromIntegral
-
-divisors :: Int -> [Int]
-divisors n = divisors' n (intSqrt n)
-
-divisors' :: Int -> Int -> [Int]
-divisors' n x
-	| x == 1 = [1]
-	| n `mod` x == 0 = x : n `div` x : divisors' n (x - 1)
-	| otherwise = divisors' n (x - 1)
+floorSqrt = floor . sqrt . fromIntegral
+ceilSqrt = ceiling . sqrt . fromIntegral
 
 divsum :: Int -> Int
-divsum = sum . divisors
+divsum n
+	| n <= 1 = 0
+	| fsqrt == ceilSqrt n = fsqrt + divsum' n (fsqrt - 1)
+	| otherwise = divsum' n fsqrt
+	where fsqrt = floorSqrt n
+
+-- If number is proper square (like 4 = 2^2 or 16 = 4^2)
+-- then this algorithm will add √x twice because of n `div` x
+-- so we check this corener case in divsum
+divsum' :: Int -> Int -> Int
+divsum' n x
+	| x == 1 = 1
+	| n `mod` x == 0 = x + n `div` x + divsum' n (x - 1)
+	| otherwise = divsum' n (x - 1)
 
 -- -- -- -- -- -- -- -- -- -- --
 
