09cc519746f74046a250a2d145a80d59f7003309
Andrew Korzhuev <wolfon@gmail.com>
2012-04-14 01:31:05 +0400

    euler 4 comments

--- euler/4.hs
+++ euler/4.hs
@@ -1,12 +1,16 @@
 -- A palindromic number reads the same both ways. 
 -- The largest palindrome made from the product of two
 -- 2-digit numbers is 9009 = 91 99.
-
 -- Find the largest palindrome made from the product of two 3-digit numbers.
 
 -- 999*999 = 998001
+-- O(x*y*z) = 9 * 10 * 7 = 630
+
+-- Because we are computing up-down, it's likely
+-- to find the solution before we'll check all palindromes
 palindromes = [x*100000 + y*10000 + z*1000 + z*100 + y*10 + x | x <- [9,8..1], y <- [9,8..0], z <- [7,6..0]]
 
+-- O(n) = 1000
 threeFactors' x 1000 = False
 threeFactors' x y = x `mod` y == 0
 					&& between (x `div` y) 100 999 
