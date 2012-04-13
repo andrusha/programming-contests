182f69d21806032585f13e47d954dc5de590ef3a
Andrew Korzhuev <wolfon@gmail.com>
2012-04-14 02:22:27 +0400

    euler 7

---
+++ euler/7.hs
@@ -0,0 +1,16 @@
+-- What is the 10 001st prime number?
+
+minus (x:xs) (y:ys) = case (compare x y) of 
+           LT -> x : minus  xs  (y:ys)
+           EQ ->     minus  xs     ys 
+           GT ->     minus (x:xs)  ys
+minus  xs     _     = xs
+
+primesPE = 2 : primes'
+  where 
+    primes' = sieve [3,5..] 9 primes'
+    sieve (x:xs) q ps@ ~(p:t)
+      | x < q     = x : sieve xs q ps
+      | otherwise =     sieve (xs `minus` [q, q+2*p..]) (head t^2) t
+
+main = putStrLn . show $ primesPE !! 10000
