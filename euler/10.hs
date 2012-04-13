0b65615988c40c08742c13c3036fdb9fe9aa5a25
Andrew Korzhuev <wolfon@gmail.com>
2012-04-14 03:32:22 +0400

    euler 10

---
+++ euler/10.hs
@@ -0,0 +1,16 @@
+-- Find the sum of all the primes below two million.
+
+minus (x:xs) (y:ys) = case (compare x y) of 
+           LT -> x : minus  xs  (y:ys)
+           EQ ->     minus  xs     ys 
+           GT ->     minus (x:xs)  ys
+minus  xs     _     = xs
+
+primesPE = 2 : primes'
+  where 
+    primes' = sieve [3,5..] 9 primes'
+    sieve (x:xs) q ps@ ~(p:t)
+      | x < q     = x : sieve xs q ps
+      | otherwise =     sieve (xs `minus` [q, q+2*p..]) (head t^2) t
+
+main = putStrLn . show $ sum . takeWhile (< 2000000) $ primesPE
